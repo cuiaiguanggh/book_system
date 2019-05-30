944bdb68a06809a55089e1b615824d395f23cff3
爱广 崔 <cuiaiguangxmh@outlook.com>
2019-05-30 20:42:05 +0800

    trivial

--- wrongBook/src/config/dataCenter.js
+++ wrongBook/src/config/dataCenter.js
@@ -2,12 +2,12 @@
 
 // const host = '192.168.10.239:80'
 // 测试服务器
- const url = "http://hw-mz-test.mizholdings.com/wrongManage";
- const host1 = "wss://hw-mz-test.mizholdings.com";
+//  const url = "http://hw-mz-test.mizholdings.com/wrongManage";
+//  const host1 = "wss://hw-mz-test.mizholdings.com";
 
 // 预备服务器
-// const url = 'https://login.kacha.xin/wrongManage-bate';
-// const host1 = "ws://login.kacha.xin";
+const url = 'https://login.kacha.xin/wrongManage-bate';
+const host1 = "ws://login.kacha.xin";
 
 
 // 正式服务器
