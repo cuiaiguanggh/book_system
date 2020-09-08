2249c3b9dbb1939d627c30823533b98349f852c0
爱广 崔 <cuiaiguangxmh@outlook.com>
2020-09-08 11:45:31 +0800

    yunke api

--- wrongBook/src/services/yukeService.js
+++ wrongBook/src/services/yukeService.js
@@ -1,14 +1,13 @@
 /*
  * @Author: your name
  * @Date: 2020-09-03 14:38:58
- * @LastEditTime: 2020-09-04 17:13:01
+ * @LastEditTime: 2020-09-08 11:39:09
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \wrongBook\src\services\yukeService.js
  */
 import requestYk from '../utils/requestYk';
-let burl = process.env.API_ENV === 't' ? "http://dayour.mizholdings.com:8080/mizhu/api/" : 'http://192.168.10.197:8088/mizhu/api/';
-// burl="http://192.168.10.197:8088/mizhu/api/"
+let burl = process.env.API_ENV === 't' ? "http://dayour.mizholdings.com:8080/mizhu/api/" : 'https://cloud.mizholdings.com/mizhu/api/'
 
 function fetchQuestions(payload) {
     return requestYk(`${burl}exam/quick?token=ad106f62a09a4e3abaf2f2e728a61c46`, {
