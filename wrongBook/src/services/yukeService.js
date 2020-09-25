f197ab63104bf759b9c490cb3bf510f148aed0bf
爱广 崔 <cuiaiguangxmh@outlook.com>
2020-09-25 14:24:10 +0800

    rt

--- wrongBook/src/services/yukeService.js
+++ wrongBook/src/services/yukeService.js
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2020-09-10 09:35:26
- * @LastEditTime: 2020-09-24 12:07:41
- * @LastEditors: your name
+ * @LastEditTime: 2020-09-25 14:15:40
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \wrongBook\src\services\yukeService.js
  */
@@ -10,6 +10,8 @@
 import store from 'store';
 import requestYk from '../utils/requestYk';
 let burl = process.env.API_ENV === 't' ? "http://dayour.mizholdings.com:8080/mizhu/api/" : 'https://cloud.mizholdings.com/mizhu/api/'
+
+let burl2 = process.env.API_ENV === 't' ? "http://dayour.mizholdings.com:8080/mizhu/api/" : 'https://api.huijudi.cn/mizhu/api/'
 let _token=store.get('wrongBookToken')
 function fetchQuestions(payload) {
     return requestYk(`${burl}exam/quick?token=${_token}`, {
@@ -18,7 +20,7 @@ function fetchQuestions(payload) {
     })
 }
 function queryQuestionsBy(payload) {
-    return requestYk(`${burl}ques/getQuestion?token=${_token}`, {
+    return requestYk(`${burl2}ques/getQuestion?token=${_token}`, {
         data: payload,
         method: 'post',
         headers:{
@@ -27,7 +29,7 @@ function queryQuestionsBy(payload) {
     })
 }
 function getZsd(payload) {
-    return requestYk(`${burl}knowbas/aftList?token=${_token}`, {
+    return requestYk(`${burl2}knowbas/aftList?token=${_token}`, {
         data: payload,
         method: 'post',
         headers:{
@@ -36,7 +38,7 @@ function getZsd(payload) {
     })
 }
 function updateQuestion(payload) {
-    return requestYk(`${burl}ques/updateQuestion?token=${_token}`, {
+    return requestYk(`${burl2}ques/updateQuestion?token=${_token}`, {
         data: payload,
         method: 'post',
         headers:{
